2801eadbfcd5b52698d8152e00382ecf9c53aa5d
Moses Finimento <mosesfinimento@googlemail.com>
2025-07-08 15:17:34 +0200

    Jetzt folgendermaßen gebuildet um den problematischen tree-shake zu umgehen und porfolio als href zu verwenden "flutter build web --base-href="/portfolio/" --no-tree-shake-icons".

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2545793100"
+    serviceWorkerVersion: "2691322547"
   }
 });
