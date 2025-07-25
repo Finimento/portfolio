9884a9adf37f44ec6796f1320c51353f23f5e652
Moses Finimento <mosesfinimento@googlemail.com>
2025-07-25 02:06:37 +0200

    ...

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1374461700"
+    serviceWorkerVersion: "3598458960"
   }
 });
